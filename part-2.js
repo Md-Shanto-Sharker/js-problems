9794066952662dad4b94b1a26a7d49819dfbad75
SHANTO <rabbymd55555@gmail.com>
2025-03-13 15:48:03 +0600

    part-2.js file

--- part-2.js
+++ part-2.js
@@ -91,3 +91,41 @@ const heights = [65, 66, 68, 72, 78, 60, 65, 66];
 
 // const shoppingCost = cartTotal(products);
 // console.log(shoppingCost);
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+function calculator(a, b, operation) {
+  if (operation === "add") {
+    const result = add(a, b);
+    return result;
+  } else if (operation === "subtract") {
+    const result = subtract(a, b);
+    return result;
+  } else if (operation === "multiply") {
+    const result = multiply(a, b);
+    return result;
+  } else if (operation === "divide") {
+    const result = divide(a, b);
+    return result;
+  } else {
+    return "not allowed";
+  }
+}
+const result = calculator(5, 7, "add");
+const result2 = calculator(5, 7, "subtract");
+const result3 = calculator(5, 7, "multiply");
+const result4 = calculator(5, 7, "divide");
+console.log(result, result2, result3, result4);
