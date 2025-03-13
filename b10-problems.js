549cc67cf8716f9f1ca188e0b6cb33a2490b1b53
SHANTO <rabbymd55555@gmail.com>
2025-03-13 19:38:15 +0600

    b10-problems.js

---
+++ b10-problems.js
@@ -0,0 +1,20 @@
+// Task-1-------------------
+// function calculateTax(income, expenses) {
+//   if (income < 0 || expenses > income || expenses < 0) {
+//     return "Invalid Input";
+//   }
+//   let parthokko = income - expenses;
+//   return parthokko * 0.2;
+// }
+// console.log(calculateTax(10000, 3000));
+// console.log(calculateTax(34000, 1753));
+// console.log(calculateTax(5000, 1500));
+// console.log(calculateTax(7000, 7000));
+// console.log(calculateTax(-5000, 2000));
+// console.log(calculateTax(6000, -1500));
+
+
+
+
+
+
